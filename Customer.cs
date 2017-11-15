662b5bb915ad54c2ca8df0bb34073ce8a6fc1c30
ktmotquin <31868769+ktmotquin@users.noreply.github.com>
2017-11-14 19:37:28 -0600

    Bare bones customer class added.

---
+++ Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAD_Project
+{
+    class Customer
+    {
+        String email, phoneNum, address;
+        void CreateAccount()
+        {
+
+        }
+
+        public void RequestMeal()
+        {
+
+        }
+    }
+}
