5b702a6ccadb1dcc2c41013367e2ef80e52f92d9
ktmotquin <31868769+ktmotquin@users.noreply.github.com>
2017-12-09 17:22:48 -0600

    Adding an error message if the user does not enter all of the correct stock info.

--- AddStock.cs
+++ AddStock.cs
@@ -33,40 +33,47 @@ namespace OOAD_Project
             bool done = false;  
             string fileName = System.IO.Path.GetFullPath(Directory.GetCurrentDirectory() + @"\\FoodItems.txt");
             var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read); // Joe's link
-                                                                                      
-            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
-            {
-                while (!done)
-                {
-                    text = streamReader.ReadLine();         // read next line
 
-                    string[] stuff = text.Split('~');
-                    if (!stuff[0].Equals("-$"))                 // Check if end of file has been reached 
-                    {
-                        if (!stuff[0].Equals("$"))
-                        {
-                            toBeWrit += text + '\n';
-                        }
-
-                    }
-                    else
-                    {
-                        done = true;                        // Item not found
-                    }
-                }
-            }
-            fileStream.Close();
-            if (!found)
+            if (String.IsNullOrEmpty(addTextBox.Text) || String.IsNullOrEmpty(quantityTxtBox.Text))
             {
-                string text1;
-                text1 = addTextBox.Text + "~" + quantityTxtBox.Text + "\n-$";
-                toBeWrit += text1;
-                System.IO.File.WriteAllText(fileName, toBeWrit);
-                this.Close();
+                MessageBox.Show("Please enter an item and a quantity.");
             }
             else
             {
-                MessageBox.Show("Item is already in database");
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    while (!done)
+                    {
+                        text = streamReader.ReadLine();         // read next line
+
+                        string[] stuff = text.Split('~');
+                        if (!stuff[0].Equals("-$"))                 // Check if end of file has been reached 
+                        {
+                            if (!stuff[0].Equals("$"))
+                            {
+                                toBeWrit += text + '\n';
+                            }
+
+                        }
+                        else
+                        {
+                            done = true;                        // Item not found
+                        }
+                    }
+                }
+                fileStream.Close();
+                if (!found)
+                {
+                    string text1;
+                    text1 = addTextBox.Text + "~" + quantityTxtBox.Text + "\n-$";
+                    toBeWrit += text1;
+                    System.IO.File.WriteAllText(fileName, toBeWrit);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Item is already in database");
+                }
             }
         }
 
@@ -77,48 +84,55 @@ namespace OOAD_Project
         {
             string toBeWrit = "";
             string text;
-            bool found = false; 
-            bool done = false;  
+            bool found = false;
+            bool done = false;
             string fileName = System.IO.Path.GetFullPath(Directory.GetCurrentDirectory() + @"\\FoodItems.txt");
-            var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read); 
-                                                                                       
-            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
-            {
-                while (!done)
-                {
-                    text = streamReader.ReadLine();         
-                    string[] stuff = text.Split('~');
-                    if (!stuff[0].Equals("-$"))                 
-                    {
-                        String[] parts = text.Split('~');      // Breaks the current line into parts
-                        if (modifyTxtBox.Text.Equals(stuff[0]))
-                        {
-                            found = true;
-                            int item1, item2;
-                            Int32.TryParse(quantityTxtBox2.Text, out item1);
-                            Int32.TryParse(stuff[1], out item2);
-                            int numitems = item1 + item2;
-                            text = stuff[0] + "~" + numitems;
-                        }
-                        toBeWrit += text + '\n';
-                    }
-                    else
-                    {
-                        done = true;                        // Info not found
-                    }
-                }
-            }
-            fileStream.Close();
-            if (found)
-            {
-                toBeWrit += "-$\n";
-                System.IO.File.WriteAllText(fileName, toBeWrit);
+            var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
 
-                MessageBox.Show("Added " + quantityTxtBox2.Text + " items of " + modifyTxtBox.Text + " to database");  
+            if (String.IsNullOrEmpty(modifyTxtBox.Text) || String.IsNullOrEmpty(quantityTxtBox2.Text))
+            {
+                MessageBox.Show("Please enter an item and a quantity.");
             }
             else
             {
-                MessageBox.Show("Error: Item not found");  // Display passwords do not match
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    while (!done)
+                    {
+                        text = streamReader.ReadLine();
+                        string[] stuff = text.Split('~');
+                        if (!stuff[0].Equals("-$"))
+                        {
+                            String[] parts = text.Split('~');      // Breaks the current line into parts
+                            if (modifyTxtBox.Text.Equals(stuff[0]))
+                            {
+                                found = true;
+                                int item1, item2;
+                                Int32.TryParse(quantityTxtBox2.Text, out item1);
+                                Int32.TryParse(stuff[1], out item2);
+                                int numitems = item1 + item2;
+                                text = stuff[0] + "~" + numitems;
+                            }
+                            toBeWrit += text + '\n';
+                        }
+                        else
+                        {
+                            done = true;                        // Info not found
+                        }
+                    }
+                }
+                fileStream.Close();
+                if (found)
+                {
+                    toBeWrit += "-$\n";
+                    System.IO.File.WriteAllText(fileName, toBeWrit);
+
+                    MessageBox.Show("Added " + quantityTxtBox2.Text + " items of " + modifyTxtBox.Text + " to database");
+                }
+                else
+                {
+                    MessageBox.Show("Error: Item not found");  // Display passwords do not match
+                }
             }
         }
 
