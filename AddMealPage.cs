773ab6b2c9941e6d8aa61b6d7e0d5388d2df8d5c
ktmotquin <31868769+ktmotquin@users.noreply.github.com>
2017-12-14 16:29:34 -0600

    Added a try catch for the image input because I didn't like how someone could enter in an incorrect filename without being notified. Once an incorrect name was entered the program would completely crash.

--- AddMealPage.cs
+++ AddMealPage.cs
@@ -20,7 +20,7 @@ namespace OOAD_Project
 
         private void Cancel_Click(object sender, EventArgs e)
         {
-           this.Close();
+            this.Close();
         }
 
         private bool checkForEmpty()
@@ -43,64 +43,78 @@ namespace OOAD_Project
 
         private void AddMeal_Click(object sender, EventArgs e)
         {
-            string replaceWith = "~";
-            string overallText = "1\n";
-            string temp, enteredInfo;
-            bool found = false; // True if an account is associated with the login info
-            bool done = false;  // True when end of user list has been reached
-            if (checkForEmpty())
+            try
             {
-                enteredInfo = MealName.Text + "$" + Description.Text.Replace("\r\n", replaceWith) + "$" + comboBox1.Text + "$"
-                                + Instructions.Text.Replace("\r\n", replaceWith) + "$" +
-                                Ingredients.Text.Replace("\r\n", replaceWith) + "$" + imgfile.Text + ".bmp";
-                System.Console.WriteLine(enteredInfo);
-                string fileName = System.IO.Path.GetFullPath(Directory.GetCurrentDirectory() + @"\\Meals.txt");
-                var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read); // Joe's link                                                                                                                        
-                //var fileStream = new FileStream(@"C:\Users\ktmot\Documents\CupOfJava\trunk\LoginInfo.txt", FileMode.Open, FileAccess.Read);
+                string filestring = imgfile.Text + ".bmp";
+                string test = Directory.GetCurrentDirectory() + @"\" + filestring;
+                Image img = Image.FromFile(test);
 
-                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+
+                string replaceWith = "~";
+                string overallText = "1\n";
+                string temp, enteredInfo;
+                bool found = false; // True if an account is associated with the login info
+                bool done = false;  // True when end of user list has been reached
+                if (checkForEmpty())
                 {
-                    while (!done)
+                    enteredInfo = MealName.Text + "$" + Description.Text.Replace("\r\n", replaceWith) + "$" + comboBox1.Text + "$"
+                                    + Instructions.Text.Replace("\r\n", replaceWith) + "$" +
+                                    Ingredients.Text.Replace("\r\n", replaceWith) + "$" + imgfile.Text + ".bmp";
+                    System.Console.WriteLine(enteredInfo);
+                    string fileName = System.IO.Path.GetFullPath(Directory.GetCurrentDirectory() + @"\\Meals.txt");
+                    var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read); // Joe's link                                                                                                                        
+                                                                                               //var fileStream = new FileStream(@"C:\Users\ktmot\Documents\CupOfJava\trunk\LoginInfo.txt", FileMode.Open, FileAccess.Read);
+
+                    using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
                     {
-                        temp = streamReader.ReadLine();         // read next line
-                        if (!temp.Equals("-1"))                 // Check if end of file has been reached 
+                        while (!done)
                         {
-                            System.Console.WriteLine("Entered text " + temp);
-                            if (!temp.Equals("1"))
+                            temp = streamReader.ReadLine();         // read next line
+                            if (!temp.Equals("-1"))                 // Check if end of file has been reached 
                             {
-                                overallText += temp + '\n';
+                                System.Console.WriteLine("Entered text " + temp);
+                                if (!temp.Equals("1"))
+                                {
+                                    overallText += temp + '\n';
+                                }
+                                if (temp.Equals(enteredInfo))
+                                {
+                                    found = true;
+                                    done = true;                    // Meal Info is unique
+                                }
                             }
-                            if (temp.Equals(enteredInfo))
+                            else
                             {
-                                found = true;
-                                done = true;                    // Meal Info is unique
+                                done = true;                        // Info not found
                             }
-                        }
-                        else
-                        {
-                            done = true;                        // Info not found
                         }
                     }
-                }
-                fileStream.Close();
-                if (!found)
-                {
-                    overallText += enteredInfo + "\n-1";
-                    System.IO.File.WriteAllText(fileName, overallText);
-                    //ID.Clear();
-                    MealName.Clear();
-                    //Type.Clear();
-                    // Keywords.Clear();
-                    imgfile.Clear();
-                    Instructions.Clear();
-                    Description.Clear();
-                    Ingredients.Clear();
-                }
-                else
-                {
-                    MessageBox.Show("Error: meal info is not unique.");  // Display meal info already exists
+                    fileStream.Close();
+                    if (!found)
+                    {
+                        overallText += enteredInfo + "\n-1";
+                        System.IO.File.WriteAllText(fileName, overallText);
+                        //ID.Clear();
+                        MealName.Clear();
+                        //Type.Clear();
+                        // Keywords.Clear();
+                        imgfile.Clear();
+                        Instructions.Clear();
+                        Description.Clear();
+                        Ingredients.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error: meal info is not unique.");  // Display meal info already exists
+                    }
                 }
             }
+            catch (FileNotFoundException error)
+            {
+                MessageBox.Show("Image not found!" + error.FileName);
+
+            }
+
         }
 
         private void btnImage_Click(object sender, EventArgs e)
@@ -117,15 +131,15 @@ namespace OOAD_Project
             }
         }
 
-      private void AddMealPage_Load(object sender, EventArgs e)
-      {
+        private void AddMealPage_Load(object sender, EventArgs e)
+        {
 
-      }
+        }
 
-      private void backToMainMenu_Click(object sender, EventArgs e)
-      {
-         this.Hide();
-      }
-   }
+        private void backToMainMenu_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
 }
 
